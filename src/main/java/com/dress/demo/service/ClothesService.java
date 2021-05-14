f2589589645c397f440e0a9abff72a9732b710db
mdorjbat <79872462+mdorjbat@users.noreply.github.com>
2021-05-14 18:02:09 -0500

    created ClothesService

--- src/main/java/com/dress/demo/service/ClothesService.java
+++ src/main/java/com/dress/demo/service/ClothesService.java
@@ -1,5 +1,10 @@
 package com.dress.demo.service;
 
+import com.dress.demo.controller.ClothesController;
+import com.dress.demo.repository.UserRepository;
+
 public class ClothesService {
-    private
+    private ClothesController clothesController;
+    private UserRepository userRepository;
+
 }
